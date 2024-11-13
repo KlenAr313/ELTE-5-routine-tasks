bca23c521eea011a477c86737c15c966d45a21af
Klenovszky Áron <aron.klenovszky@gmail.com>
2024-11-13 10:07:14 +0100

    Error handling maybe

--- Telekom/bead4/checksum_srv.py
+++ Telekom/bead4/checksum_srv.py
@@ -13,15 +13,16 @@ with socket(AF_INET, SOCK_STREAM) as server:
     server.bind(server_addr)
     server.listen()
     server.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    server.settimeout(1.0)
 
     sockets = [server]
     validations = {}
     cur_time = last_time = time()
 
     while True:
-        readables, writeables, e = select(sockets, [], [], 1)
+        readables, writeables, ex = select(sockets, [], [], 1)
 
-        if not (readables or writeables or e):
+        if not (readables or writeables or ex):
             continue
 
         for s in readables:
@@ -38,18 +39,24 @@ with socket(AF_INET, SOCK_STREAM) as server:
                     #validations[key][1] = int(validations[key][1]) - floor(cur_time - last_time)
                     if cur_time - last_time > validations[key][1]:
                         del validations[key]
+                try:
+                    data = s.recv(256)
+                    if data:
+                        msg = data.decode().split('|')
+                        print(data.decode())
+                        if msg[0] == 'BE':
+                            validations[msg[1]] = (msg[1], int(msg[2]), int(msg[3]), msg[4]) #fileId, time, size, bytes
+                            s.send(b'OK')
+                        elif msg[0] == 'KI':
+                            if msg[1] in validations.keys():
+                                print('|'.join([str(validations[msg[1]][2]), str(validations[msg[1]][3])]))
+                                s.send(bytes('|'.join([str(validations[msg[1]][2]), str(validations[msg[1]][3])]), encoding='utf-8'))
+                            else: s.send(b'|0')
+                except:
+                    print()
 
-                data = s.recv(256)
-                if data:
-                    msg = data.decode().split('|')
-                    print(data.decode())
-                    if msg[0] == 'BE':
-                        validations[msg[1]] = (msg[1], int(msg[2]), int(msg[3]), msg[4]) #fileId, time, size, bytes
-                        s.send(b'OK')
-                    elif msg[0] == 'KI':
-                        if msg[1] in validations.keys():
-                            print('|'.join([str(validations[msg[1]][2]), str(validations[msg[1]][3])]))
-                            s.send(bytes('|'.join([str(validations[msg[1]][2]), str(validations[msg[1]][3])]), encoding='utf-8'))
-                        else: s.send(b'|0')
+        for e in ex:
+            sockets.remove(e)
+            e.close()
 
                 
