7a835cc86d8a527bf9be152c8b20ba4c3935608c
Klenovszky Áron <aron.klenovszky@gmail.com>
2024-11-28 20:59:25 +0100

    MatStat - Fetching data, reading it in

---
+++ MatStat/Bead/bead.R
@@ -0,0 +1,6 @@
+#install.packages("readxl")
+library("readxl")
+incU1M22 <- read_excel("./MatStat/Bead/TEIR_inc_u_1m_22.xlsx")
+egyenei = read_excel("./MatStat/Bead/Egyéni_szavazás_szkjkv_22.xlsx")
+incU1M22
+egyenei
